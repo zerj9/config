6c0255f50e41d485045a74c39a90bd054ec54a7e
zerj9 <96551236+zerj9@users.noreply.github.com>
2026-02-03 15:00:56 +0000

    git alias

--- fish/config.fish
+++ fish/config.fish
@@ -3,10 +3,17 @@ if status is-interactive
   alias vim='nvim'
   alias l='ls -alh --color=auto'
   alias gst='git status'
+  alias gco='git checkout'
+  alias gd='git diff'
+  alias gds='git diff --staged'
   alias gl='git pull'
   alias ga='git add'
   alias gau='git add -u'
   alias gcm='git commit -m'
   alias gp='git push'
   alias gpsup='git push --set-upstream origin (git rev-parse --abbrev-ref HEAD)'
+end
+
+function fish_title
+    # Do nothing - prevents fish from changing tmux window names
 end
